5f342645445897c91394bb956dee699f56bd748a
Peter LaMontagne <plamon42@gmail.com>
2021-02-09 00:30:35 -0800

    added comments, minor style changes

--- activity.cpp
+++ activity.cpp
@@ -3,31 +3,35 @@
 using std::vector;
 using std::cout;
 
+
+/*
+ * Driver function for activity selection. This reads the data in from act.txt, sorts
+ * it in decresing start time order, than runs the activity selection algorithm on it, 
+ * printing the results to the terminal
+ */
 int main() {
-    
+    //read in data
     vector<vector<int>> data;
     read_file(data);
 
-    auto line_in_file = 0;
+    int line_in_data = 0;
     int set = 0;
 
-    //T Test Cases  (first int in file)
+    //For each test case
     do {
         //get length of data set, advance to next line
-        int data_size = data[line_in_file++][0];
+        int data_size = data[line_in_data++][0];
 
         //make temp vector for storing specific data
         vector<activity> activities(data_size);
         
         //read each data set into data_set
         for(int i = 0; i < data_size; i++) {
-            activities[i].activity_number = data[line_in_file][0];
-            activities[i].start_time = data[line_in_file][1];
-            activities[i].end_time = data[line_in_file][2];
-            line_in_file++;
+            activities[i].activity_number = data[line_in_data][0];
+            activities[i].start_time = data[line_in_data][1];
+            activities[i].end_time = data[line_in_data][2];
+            line_in_data++;
         }
-
-        cout << "\nSet: " << set << "\n";
 
         //sort data
         merge_sort(activities, 0, activities.size() - 1);
@@ -36,6 +40,7 @@ int main() {
         vector<activity> selected = activity_selection(activities);
 
         //print results
+        cout << "\nSet: " << set << "\n";
         cout << "Number of activities selected: " << selected.size() << "\nActivities: ";
         for(int i = (int)selected.size() - 1; i >= 0; i--) {
             cout << selected[i].activity_number << " ";
@@ -43,19 +48,30 @@ int main() {
         cout << "\n";
 
         set++;
-    } while (line_in_file < (int)data.size());
+    } while (line_in_data < (int)data.size());
 }
 
 
 
-vector<activity> activity_selection(vector<activity> activities) {
-    vector<activity> selected;
-    int total_activities = activities.size();
 
+/*
+ * This function takes a vector of activity structs and selects the maximum
+ * number it can without overlapping.
+ * 
+ * Complexity: Θ(n)
+ *
+ * Params:
+ *   activities - a vector of activities that are sorted in decresing start time
+ */
+vector<activity> activity_selection(vector<activity> activities) {
+    //vector for storing selected activities
+    vector<activity> selected;
+
+    //always select the first activity (last to start)
     selected.push_back(activities[0]);
 
     //loop thru all activies sorted by decresing start time
-    for(int m = 1; m < total_activities; m++) {
+    for(int m = 1; m < (int)activities.size(); m++) {
         //if activity ends before existing activity starts, add it
         if(activities[m].end_time <= selected[selected.size() - 1].start_time) {
             selected.push_back(activities[m]);
